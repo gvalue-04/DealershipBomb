2984e2e0c050123838ad653ce2a7c09ead69447a
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-07-10 23:06:10 -0400

    oopsie

--- BombServer/Properties/AssemblyInfo.cs
+++ BombServer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dealership")]
+[assembly: AssemblyTitle("BombServerEmu_MNR")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dealership")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyProduct("BombServerEmu_MNR")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
